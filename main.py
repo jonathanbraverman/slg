119a21ce6c80f83ae618803885b97a82a7a54be3
jonathanbraverman <109186475+jonathanbraverman@users.noreply.github.com>
2023-01-26 17:05:14 -0500

    about to remove gameboard future

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import event as event
 gamepiece = "|"
 bg = " "
 boardwidth = 30
+previewsize = 4
 gameboard_past = [bg for x in range(0,boardwidth)]
-gameboard_future = [list() for preview in range(0,4)]
-for preview in range(0,4):
-    gameboard_future[preview] = [bg for x in range(0, boardwidth)]
+gameboard_future = [[bg for x in range(0, boardwidth)] for preview in range(0,previewsize)]
 pieceposition = 3
 max_position = len(gameboard_past)
 
 gameinprogress = True
 
 while gameinprogress:
-    time.sleep(0.10)
+    time.sleep(0.05)
     events = event.get_events()
 
     if "left arrow" in events:
@@ -24,7 +23,7 @@ while gameinprogress:
         gameboard_past[pieceposition] = gamepiece
         
     if "right arrow" in events:
-        if pieceposition < max_position:
+        if pieceposition < max_position - 1:
             gameboard_past[pieceposition] = bg
             pieceposition = pieceposition + 1
         gameboard_past[pieceposition] = gamepiece
@@ -33,3 +32,5 @@ while gameinprogress:
         gameinprogress = False
 
     print(*gameboard_past, sep='')
+    #for line_preview in gameboard_future:
+    #    print(*line_preview, sep='')
